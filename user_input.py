d0cc5978b65cca391883e14e7a2621ead470e581
Davoste <95017584+Davoste@users.noreply.github.com>
2024-05-23 15:58:21 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Ask the user for their name and store it in the variable 'name'
+name = input("Please enter your name: ")
+age = input("What is your age: ")
+location = input("what is your location: ")
+
+# Print a greeting message
+print(f"Hello, {name} you are {age} years old and currently located in {location}!")
